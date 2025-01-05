3d366a1d46c4035027c4a818f89ac527e940d6e9
feselene <vaporflyb@gmail.com>
2025-01-05 14:47:08 -0500

    Add dodgeswitch xp farm bot script

---
+++ xp.py
@@ -0,0 +1,24 @@
+import pydirectinput
+import time
+
+def main():
+    time.sleep(1)
+    while True:
+        # Press 'R' key once
+        pydirectinput.press('r')
+        print("Pressed 'R'")
+        time.sleep(2)
+        pydirectinput.keyDown('t')
+        time.sleep(1)
+        pydirectinput.keyUp('t')
+        for i in range(40):
+            pydirectinput.keyDown('d')
+            time.sleep(0.5)
+            pydirectinput.keyUp('d')   
+            pydirectinput.keyDown('a')
+            time.sleep(0.5)
+            pydirectinput.keyUp('a')  
+        time.sleep(5)  # Adjust as needed
+
+if __name__ == "__main__":
+    main()
